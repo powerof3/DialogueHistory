115fe89e3bf9e9ed227d364e3db2e81fea1cfdaf
powerof3 <powerof3@users.noreply.github.com>
2024-09-28 23:19:44 +0530

    Disable default logger

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 
 	logger::info("Game version : {}", a_skse->RuntimeVersion().string());
 
-	SKSE::Init(a_skse);
+	SKSE::Init(a_skse, false);
 
 	ImGui::Renderer::Install();
 
